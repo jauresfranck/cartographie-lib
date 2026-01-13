23452bc2ea8b918afa3c0b1318439a93f2e9cb52
joeldt <tapajoel16@gmail.com>
2026-01-13 10:58:32 +0100

    Ajout nombre de quais

--- cartographie-lib/src/main/java/fr/cours/info/tp/ferroviaire/cartographie/models/Gare.java
+++ cartographie-lib/src/main/java/fr/cours/info/tp/ferroviaire/cartographie/models/Gare.java
@@ -1,4 +1,13 @@
 package fr.cours.info.tp.ferroviaire.cartographie.models;
 
 public class Gare {
+    private int nombreQuais;
+
+    public int getNombreQuais() {
+        return nombreQuais;
+    }
+
+    public void setNombreQuais(int nombreQuais) {
+        this.nombreQuais = nombreQuais;
+    }
 }
