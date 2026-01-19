6935d535b421474fa1071ad83a5367804aad9d26
Joel TCHINDA_TAPA <e2405720@ensimlan.univ-lemans.fr>
2026-01-19 16:38:12 +0100

    Ma version de la logique ferroviaire

--- cartographie-lib/src/main/java/fr/cours/info/tp/ferroviaire/cartographie/models/Gare.java
+++ cartographie-lib/src/main/java/fr/cours/info/tp/ferroviaire/cartographie/models/Gare.java
@@ -8,7 +8,7 @@ public class Gare {
     }
 
     public void setNombreQuais(int nombreQuais) {
-        this.nombreQuais = nombreQuais;
+        this.nombreQuais = nombreQuais;}
 
     private String nom;
 
